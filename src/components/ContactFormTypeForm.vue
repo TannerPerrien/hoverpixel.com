9652ccaa7d2d2cda03d4115836577bc424475b04
Tanner Perrien <weslee3@gmail.com>
2019-05-29 22:49:41 -0700

    Switch to TypeForm

---
+++ src/components/ContactFormTypeForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="typeform-widget" data-url="https://hoverpixel.typeform.com/to/sfVCOC" style="width: 100%; height: 500px;" />
+</template>
+
+<script>
+  export default {
+    mounted() {
+      (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
+    }
+  }
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,18 +4,21 @@
       <v-flex>
         <h1 class="display-1">Contact</h1>
         <p>Send us a message and we will get back to you within 24 hours.</p>
-        <ContactForm />
+        <!-- <ContactForm /> -->
+        <ContactFormTypeForm />
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  import ContactForm from '@/components/ContactForm'
+  // import ContactForm from '@/components/ContactForm'
+  import ContactFormTypeForm from '@/components/ContactFormTypeForm'
 
   export default {
     components: {
-      ContactForm
+      // ContactForm
+      ContactFormTypeForm
     }
   }
 </script>
