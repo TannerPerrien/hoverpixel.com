3506615201654316e37cb7f600a53c96ea9fea16
Tanner Perrien <weslee3@gmail.com>
2019-06-01 09:10:56 -0700

    Update services

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -28,12 +28,12 @@
     data: function() {
       return {
         services: [
-          { key: 'real-estate', name: 'Real Estate', summary: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.' },
-          { key: 'utilities', name: 'Utilities', summary: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.' },
-          { key: 'construction', name: 'Construction', summary: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.' },
-          { key: 'civil-engineering', name: 'Civil Engineering', summary: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.' },
-          { key: 'cinematography', name: 'Cinematography', summary: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.' },
-          { key: 'consulting', name: 'Consulting', summary: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.' },
+          { key: 'real-estate', name: 'Real Estate', summary: 'We all know how important high quality photos are when it comes to marketing real estate, but are you missing a higher perspective?  We can provide you with high resolution, highly detailed aerial photography for your listings that will separate you from the rest of the pack.  Packages start at $99.' },
+          { key: 'utilities', name: 'Utilities', summary: 'The days of climbing ladders to inspect towers visually or with the aid of instrumentation are over.  Our fleet of unmanned aircraft can get there, view and record, and move on before a human would have time to don the required safety gear.  We can provide high resolution imagry or thermal imagery to give you the data you need.' },
+          { key: 'construction', name: 'Construction', summary: 'From aerial site surveys to code inspections to documenting for posterity, being able to capture imagery from above is a necessity.  Allow us to capture these images at a fraction of the cost and imposition of hiring other aerial photography and inspection such has helicopters or climbers.' },
+          { key: 'civil-engineering', name: 'Civil Engineering', summary: 'Drones are able to capture top down imagery or get a vantage point that is not easily achieved by ground photography.  Drone videography can be helpful in studying anything from traffic patterns to wildlife activity.' },
+          { key: 'cinematography', name: 'Cinematography', summary: 'Add aerial video to your existing footage OR have us shoot an entire project.  Our footage is compelling, silky smooth, and produced at 4K resolution!' },
+          { key: 'consulting', name: 'Consulting', summary: 'Do you want to fly your own drone or take a test flight with one of ours?  Would you like some guidance on FAA regulation when it comes to air space or protocol?  We are here for you!' },
         ]
       }
     }
